04a6c5980679dc8cbe73ccd815137a79fb3e3137
Nawan743 <nawandx743@gmail.com>
2021-05-12 20:19:30 -0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export default class Index extends React.Component {
+  state = {
+    weather: []
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  componentDidMount() {
+    axios.get(`https://goweather.herokuapp.com/weather/Uberlandia`)
+      .then(res => {
+        const weather = res.data.forecast;
+        this.setState({ weather });
+        console.log(weather)
+      })
+      .catch(e => console.log(e));
+  }
+  
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Previsão do tempo em Uberlândia nos próximos dias</h1>
+        <table>
+          <tr>
+            <th>Dia</th>
+            <th>Temperatura</th>
+            <th>Vento</th>
+          </tr>
+          {this.state.weather.map(prevision =>
+            <React.Fragment>
+              <tr>
+                <td>{prevision['day']}</td>
+                <td>{prevision['temperature'].replace('+', '')}</td>
+                <td>{prevision['wind']}</td>
+              </tr>
+            </React.Fragment>
+          )}
+        </table>
+      </React.Fragment>
+    )
+  }
+}
+
+ReactDOM.render(<Index></Index>, document.getElementById('root'));
